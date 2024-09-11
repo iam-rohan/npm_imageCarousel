4021e86216eaf96c91d516fda0c72f7b8a195023
iam-rohan <rohanacharya42@gmail.com>
2024-09-11 21:51:59 +0545

    Add required assets and styling with dynamic carouselDot insertion logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
-const container = document.querySelector(".images-container");
+function carouselDots(images, mainContainer) {
+  const dotCount = document.querySelectorAll(images);
 
-container.style.backgroundColor = "yellow";
+  const container = document.querySelector(mainContainer);
+
+  const carouselDots = document.createElement("div");
+  carouselDots.classList.add("carousel-dots");
+
+  dotCount.forEach((_, index) => {
+    const carouselDot = document.createElement("span");
+    carouselDot.classList.add("carousel-dot");
+    if (index === 0) {
+      carouselDot.classList.add("active");
+    }
+    carouselDots.appendChild(carouselDot);
+  });
+
+  container.appendChild(carouselDots);
+}
+
+carouselDots(".carousel-image", ".carousel-container");
